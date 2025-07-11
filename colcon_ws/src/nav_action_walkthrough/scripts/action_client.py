aa8919b31aa16edd41a9feb554b56de50bbe2e61
Rsleiman <ralphsleiman16@gmail.com>
2025-07-11 14:14:05 +0100

    Finished action server and client. Requires manual publishing on /robot_position topic in runtime to update

---
+++ colcon_ws/src/nav_action_walkthrough/scripts/action_client.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from rclpy.action.server import ServerGoalHandle
+from nav_action_walkthrough.action import Navigate
+from geometry_msgs.msg import Point
+from rclpy.task import Future
+
+DISTANCE_THRESHOLD = 0.4 # m
+
+class NavigateActionClient(Node):
+    def __init__(self):
+        super().__init__("action_client_node")
+        self._action_client = ActionClient(self, Navigate, "navigate")
+        print("Action Client running...")
+
+
+    def send_goal(self, x, y, z):
+        goal_msg = Navigate.Goal()
+        goal_msg.goal_point.x = float(x)
+        goal_msg.goal_point.y = float(y)
+        goal_msg.goal_point.z = float(z)
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback) # To see if server accepts or rejects our goal
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        print(f"Feedback: {feedback.distance_to_point}")
+
+    def goal_response_callback(self, future: Future):
+        goal_handle = future.result()
+        if goal_handle.accepted == False:
+            print("Goal Rejected...")
+            return None
+        
+        print("Goal Accepted")
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback) # To see what the result of the accepted goal is once accomplished
+
+    def get_result_callback(self, future: Future):
+        result = future.result().result
+        print(f"Result: {result.elapsed_time} seconds")
+        rclpy.shutdown()
+
+
+
+def main(args=None):
+    rclpy.init()
+    action_client_node = NavigateActionClient()
+
+    try:
+        x = input("Enter X coordinate: ")
+        y = input("Enter Y coordinate: ")
+        z = input("Enter Z coordinate: ")
+
+        action_client_node.send_goal(x, y, z)
+        rclpy.spin(action_client_node)
+    except KeyboardInterrupt:
+        print("Terminating Node...")
+        action_client_node.destroy_node()
+
+
+if __name__ == "__main__":
+    main()
--- colcon_ws/src/nav_action_walkthrough/scripts/action_server.py
+++ colcon_ws/src/nav_action_walkthrough/scripts/action_server.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python3
-
+from math import dist
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
+from rclpy.action.server import ServerGoalHandle
 from nav_action_walkthrough.action import Navigate
+from geometry_msgs.msg import Point
 
+DISTANCE_THRESHOLD = 0.05 # m
 
 class NavigateActionServer(Node):
     def __init__(self):
         super().__init__("action_server_node")
         self._action_server = ActionServer(self, Navigate, "navigate", self.navigate_callback)
-
-
-        
+        self.position_sub = self.create_subscription(Point, "robot_position", self.update_robot_position, 1)
+        self.robot_current_position = None
         print("Action Server running...")
 
-    def navigate_callback(self, goal_handle):
+    def navigate_callback(self, goal_handle:ServerGoalHandle):
         print("Received goal")
+        start_time = self.get_clock().now().to_msg().sec
         self.robot_goal_point = [goal_handle.request.goal_point.x,
                                  goal_handle.request.goal_point.y,
                                  goal_handle.request.goal_point.z]
+        print(f"Goal point: {self.robot_goal_point}")
+
+        while not self.robot_current_position:
+            print("Waiting for robot position to be detected")
+            rclpy.spin_once(self, timeout_sec=2) # spin_once does not stop rest of class from running
+
+        self.distance_to_goal = dist(self.robot_current_position, self.robot_goal_point)
+        feedback_msg = Navigate.Feedback()
+
+        print(self.distance_to_goal)
+        while self.distance_to_goal > DISTANCE_THRESHOLD:
+            self.distance_to_goal = dist(self.robot_current_position, self.robot_goal_point)
+            feedback_msg.distance_to_point = self.distance_to_goal
+            goal_handle.publish_feedback(feedback_msg)
+            rclpy.spin_once(self, timeout_sec=0.5)
+
+        goal_handle.succeed()
+        result = Navigate.Result()
+        result.elapsed_time = float(self.get_clock().now().to_msg().sec - start_time)
+
+        return result
+
+    
+    def update_robot_position(self, point_msg: Navigate.Goal):
+        self.robot_current_position = [point_msg.x, point_msg.y, point_msg.z]
+        
 
 def main(args=None):
     rclpy.init()
     action_server_node = NavigateActionServer()
 
     try:
-        rclpy.spin(action_server_node)
+        while rclpy.ok():
+            rclpy.spin_once(action_server_node)
     except KeyboardInterrupt:
+        print("Terminating node...")
+        action_server_node._action_server.destroy()
         action_server_node.destroy_node()
 
 
